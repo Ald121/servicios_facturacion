2597b1f7d3aa1d5f05c78ecec49fbf4eb3189826
Ald121 <fallsouls@hotmail.com>
2017-01-03 17:39:21 -0500

    ingreso al sistema
    creacion de rutas, crear marcas, modelos, proveedores, productos

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -4,14 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
-
+use Hash;
+//------------------------------------ Autenticacion --------------------
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 class loginController extends Controller
 {
     public function __construct(){
-    	
+
     }
 
     public function Acceso(Request $request){
+         //return response()->json(["respuesta"=>$request->all()]);
+        $datos=DB::table('usuarios.usuarios')->select('nick','id')->where('nick',$request->nick)->first();
 
+        if (count($datos)==0) {
+            return response()->json(["respuesta"=>false]);
+        }
+
+        $datos=DB::table('usuarios.usuarios')->select('clave_clave')->where('id',$datos->id)->first();
+        $checkpass=Hash::check($request->clave, $datos->clave_clave);
+
+        if ($checkpass) {
+         $datos = DB::table('usuarios.usuarios')->select('id','nick')->where('nick',$request->nick)->first();
+         // $extra=['nbdb'=>$name_bdd,'pnb'=>$pass_bdd,'ruc'=>$acceso->nick];
+         // $token = JWTAuth::fromUser($datos,$extra);
+         $token = JWTAuth::fromUser($datos);
+         $datosUser=DB::table('usuarios.usuarios')->select('id','nick')->where('nick',$request->nick)->first();
+         //DB::table('usuarios.usuarios')->where('nick',$user)->update(["token"=>$token]);
+         return response()->json(['respuesta'=>true,'token'=>$token,'datosUser'=>$datosUser]);
+        }
+        return response()->json(["respuesta"=>$checkpass]);
+        //return response()->json(["respuesta"=>$name_bdd]);
+           
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,35 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'cors'], function(){
-	Route::get('Login','loginController@Acceso');
+	Route::post('Acceso','loginController@Acceso');
 
 	Route::group(['middleware' => ['jwt.auth']], function ()
         {
+        	//Categorias 
+        	Route::post('Add_Categoria','caregoriasController@Add_Categoria');
+        	Route::post('Update_Categoria','caregoriasController@Update_Categoria');
+        	Route::post('Delete_Categoria','caregoriasController@Delete_Categoria');
+        	Route::post('Get_Marcas','caregoriasController@Get_Marcas');
+        	//Marcas 
+        	Route::post('Add_Marcas','MarcasController@Add_Marcas');
+        	Route::post('Update_Marcas','MarcasController@Update_Marcas');
+        	Route::post('Delete_Marcas','MarcasController@Delete_Marcas');
+        	Route::post('Get_Marcas','MarcasController@Get_Marcas');
+        	//Modelos 
+        	Route::post('Add_Modelos','ModelosController@Add_Modelos');
+        	Route::post('Update_Modelos','ModelosController@Update_Modelos');
+        	Route::post('Delete_Modelos','ModelosController@Delete_Modelos');
+        	Route::post('Get_Modelos','ModelosController@Get_Modelos');
+        	//Productos 
+        	Route::post('Add_Productos','ProductosController@Add_Productos');
+        	Route::post('Update_Productos','ProductosController@Update_Productos');
+        	Route::post('Delete_Productos','ProductosController@Delete_Productos');
+        	Route::post('Get_Productos','ProductosController@Get_Productos');
+        	//Proveedores 
+        	Route::post('Add_Proveedores','ProveedoresController@Add_Proveedores');
+        	Route::post('Update_Proveedores','ProveedoresController@Update_Proveedores');
+        	Route::post('Delete_Proveedores','ProveedoresController@Delete_Proveedores');
+        	Route::post('Get_Proveedores','ProveedoresController@Get_Proveedores');
 	
 		});
 });
